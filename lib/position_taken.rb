2d465533813f073e25b08dbc3f200c2b0d65283c
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-10 15:37:25 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  place = board[index].strip
+  place = board[index].to_s.strip
   case place
   when "X"
     return true
